27a7a143615732d5e3db346145e70e9e9eaebc7e
Mislav Franjo <mislav.franja1@gmail.com>
2019-09-09 19:48:51 +0200

    metode napiso izvan klase

--- main.cpp
+++ main.cpp
@@ -11,55 +11,46 @@ public:
 
 class MyWindow : public Window {
 
-	static WNDPROC origEditWndProc;
-
-public:
-	MyWindow() {};
 protected:
-	int OnCreate(CREATESTRUCT* psc)
+	int OnCreate(CREATESTRUCT* pcs);
+	void OnCommand(int id);
+	void OnDestroy();
+};
+
+int MyWindow::OnCreate(CREATESTRUCT* psc)
+{
+	Button start, setting, quit;
+	char s[128];
+
+	LoadString(0, IDS_STRING101, s, sizeof s);
+	start.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, s, IDC_START, 150, 150, 200, 100);
+
+	LoadString(0, IDS_STRING102, s, sizeof s);
+	setting.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, s, IDC_SETTING, 400, 150, 200, 100);
+
+	LoadString(0, IDS_STRING103, s, sizeof s);
+	quit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, s, IDC_QUIT, 575, 500, 200, 50);
+
+	return 0;
+}
+void MyWindow::OnCommand(int id)
+{
+	switch (id)
 	{
-		Button start, setting, quit;
-		char s[128];
-
-		LoadString(0, IDS_STRING101, s, sizeof s);
-		start.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, s, IDC_START, 150, 150, 200, 100);
-
-		LoadString(0, IDS_STRING102, s, sizeof s);
-		setting.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, s, IDC_SETTING, 400, 150, 200, 100);
-
-		LoadString(0, IDS_STRING103, s, sizeof s);
-		quit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, s, IDC_QUIT, 575, 500, 200, 50);
-
-		return 0;
-	}
-	void OnCommand(int id)
-	{
-		switch (id)
-		{
 		/*case IDC_START:
 			return 0;
 		case IDC_SETTING:
 			return 0;*/
-		case IDC_QUIT:
-			printf("qwer");
-		}
+	case IDC_QUIT:
+		PostQuitMessage(0);
 	}
+}
 
-	void OnDestroy()
-	{
-		::PostQuitMessage(0);
-	}
+void MyWindow::OnDestroy()
+{
+	::PostQuitMessage(0);
+}
 
-	static LRESULT CALLBACK EditWndProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
-	{
-		if (msg == WM_CHAR)
-			return 0;
-		return CallWindowProc(origEditWndProc, hw, msg, wp, lp);
-	}
-
-};
-
-WNDPROC MyWindow::origEditWndProc;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
