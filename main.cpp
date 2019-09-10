b2b5d292cb9f9d5ca434748e0a51ab0956ecbce2
Mislav Franjo <mislav.franja1@gmail.com>
2019-09-10 16:56:12 +0200

    dodan dialog za setting

--- main.cpp
+++ main.cpp
@@ -10,6 +10,29 @@ public:
 	std::string ClassName() { return "BUTTON"; }
 };
 
+INT_PTR CALLBACK AboutDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	switch (Message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_Apply:
+			EndDialog(hwnd, IDOK);
+			break;
+		case IDC_BUTTON1:
+			EndDialog(hwnd, IDC_CANCLE);
+			break;
+		}
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
+
 class MyWindow : public Window {
 
 protected:
@@ -36,14 +59,17 @@ int MyWindow::OnCreate(CREATESTRUCT* psc)
 }
 void MyWindow::OnCommand(int id)
 {
+	HWND hDlg;
+
 	switch (id)
 	{
 	case IDC_START:
 		MessageBox(NULL, "start", NULL, MB_OK);
 			break;
 	case IDC_SETTING:
-		MessageBox(NULL, "settings", NULL, MB_OK);
-			break;
+		hDlg = CreateDialog(GetModuleHandle(NULL),MAKEINTRESOURCE(IDD_DIALOG2),NULL,AboutDlgProc);
+		ShowWindow(hDlg, SW_SHOW);
+		break;
 	case IDC_QUIT:
 		PostQuitMessage(0);
 		break;
--- resource1.h
+++ resource1.h
@@ -2,9 +2,9 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDC_START	001
+#define IDC_START                       001
 #define IDC_SETTING                     002
-#define IDC_QUIT                     003
+#define IDC_QUIT                        003
 #define IDS_STRING101                   101
 #define IDS_STRING102                   102
 #define IDS_STRING103                   103
@@ -13,6 +13,11 @@
 #define IDD_DIALOG1                     109
 #define IDD_DIALOG2                     112
 #define IDC_LIST1                       1002
+#define IDC_CANCLE                      1003
+#define IDC_OK                          1003
+#define IDC_Apply                       1003
+#define IDCANCLE_SETT                   1004
+#define IDC_BUTTON1                     1005
 
 // Next default values for new objects
 // 
@@ -20,7 +25,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        117
 #define _APS_NEXT_COMMAND_VALUE         40010
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
